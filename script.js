6c284e402c78f741b9e63130a860536d0f674b67
marquezmaxi <107836391+marquezmaxi@users.noreply.github.com>
2022-06-20 06:45:20 -0300

    Added all files

---
+++ script.js
@@ -0,0 +1,12 @@
+let n1 = prompt('Ingresa el primer número');
+let n2 = prompt('Ingresa el segundo número');
+
+if (n1 > n2) {
+    alert('gay');
+}
+
+parseInt(n1);
+parseInt(n2);
+
+var resultado = n1 + n2;
+alert(resultado); 
